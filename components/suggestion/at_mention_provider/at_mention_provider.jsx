d4fe1148c351bccfae96b9f6cbc29c002b0fd160
Vyacheslav Zamyatin <vyzamyatin@gmail.com>
2020-09-09 11:26:43 +0300

    [VZ] remove non-members from at-mention suggestion

--- components/suggestion/at_mention_provider/at_mention_provider.jsx
+++ components/suggestion/at_mention_provider/at_mention_provider.jsx
@@ -164,6 +164,9 @@ export default class AtMentionProvider extends Provider {
     // remoteNonMembers matches users listed as not in the channel by the server.
     // listed in the channel from local results.
     remoteNonMembers() {
+        return [];
+
+        /*
         if (!this.data) {
             return [];
         }
@@ -174,6 +177,7 @@ export default class AtMentionProvider extends Provider {
                 type: Constants.MENTION_NONMEMBERS,
                 ...profile,
             }));
+        */
     }
 
     items() {
